10856483c199e1fe5d7d18628c3669c7089a5f09
danielrjilek <jilek.d@northeastern.edu>
2025-05-10 15:11:05 -0500

    getComputerChoice and getHumanChoice functions created in script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("Hello World");
+
+function getComputerChoice() {
+    let choice = Math.random();
+    if (choice < 0.33) {
+        return "rock";
+    }
+    else if (choice < 0.67) {
+        return "paper";
+    }
+    else return "scissors";
+}
+
+function getHumanChoice() {
+    return prompt("Your choice:");
+}
