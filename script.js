2c018ba6aeaf4600be403198648a7d2fdcd4d588
danielrjilek <jilek.d@northeastern.edu>
2025-05-10 15:23:18 -0500

    determineWinner function added to script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 console.log("Hello World");
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     let choice = Math.random();
@@ -13,4 +15,43 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     return prompt("Your choice:");
+}
+
+function determineWinner(humanSelection, computerSelection) {
+    if (humanSelection == computerSelection) {
+        return "tie";
+    }
+    else if (humanSelection == "rock") {
+        if (computerSelection == "paper") {
+            return "computer";
+        }
+        else {
+            return "human";
+        }
+    }
+    else if (humanSelection == "paper") {
+        if (computerSelection = "scissors") {
+            return "computer";
+        }
+        else {
+            return "human";
+        }
+    }
+    else if (humanSelection = "scissors") {
+        if (computerSelection = "rock") {
+            return "computer";
+        }
+        else {
+            return "human";
+        }
+    }
+}
+
+function playRound() {
+    let winner = null;
+    const humanSelection = getHumanChoice().toLowerCase();
+    const computerSelection = getComputerChoice();
+    console.log(`Computer chooses ${computerSelection}`);
+    winner = determineWinner(humanSelection, computerSelection);
+    
 }
